f24b6c407485cab944c31c8b5919fbacf76f099a
stationstops <chris@schoenfeld.com>
2013-05-19 21:26:19 -0400

    Java simple example

---
+++ java/HelloWorld.java
@@ -0,0 +1,21 @@
+/* HellowWorld.java */
+
+// Filename = case-sensitive ClassName
+
+// Compile to Java bytecode with the java compiler
+// $javac HelloWorld.java 
+
+// This will create a HelloWorld.class file
+// Execute the file with the Java runtime, with the classname only
+
+// $ java HelloWorld
+// $ Hello, World
+
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
